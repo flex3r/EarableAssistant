034dbc03891d96e3bdc2d63c46c3f7d1e5940ff3
Felix <felixschmidbb@gmail.com>
2020-05-11 16:27:57 +0200

    Initial commit

---
+++ app/src/main/java/com/flxrs/earablecompass/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.flxrs.earablecompass
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.flxrs.earablecompass.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity(R.layout.main_activity) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/com/flxrs/earablecompass/ui/main/MainFragment.kt
@@ -0,0 +1,22 @@
+package com.flxrs.earablecompass.ui.main
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import com.flxrs.earablecompass.R
+
+class MainFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = MainFragment()
+    }
+
+    private val viewModel: MainViewModel by viewModels()
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        return inflater.inflate(R.layout.main_fragment, container, false)
+    }
+}
